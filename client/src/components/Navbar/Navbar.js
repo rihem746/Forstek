894858b533cfc37fbf98e7a4b77b1d98c3ee064b
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2022-04-09 00:05:26 +0100

    user-name

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -38,8 +38,10 @@ const Navbar = () => {
       <Toolbar className={classes.toolbar}>
         {user ? (
           <div className={classes.profile}>
+            <div className={classes.profileData}>
             <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+            </div>
             <Button variant="conatined"  onClick={logout} className={classes.logout}>se déconnecter</Button>
           </div>
         ):(
